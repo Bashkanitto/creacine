57bef25d88bd9a025ea19042e107d6f504acda4e
aidyn <aidyn.meiramkhanuly@gmail.com>
2025-06-07 03:33:22 +0500

    feat: add CustomInput component

---
+++ src/components/CustomInput.tsx
@@ -0,0 +1,29 @@
+import { colors } from '@/constants/colors'
+
+export function CustomInput({
+  type,
+  placeholder,
+  className,
+}: {
+  type: string
+  placeholder?: string
+  className?: string
+}) {
+  return (
+    <input
+      type={type}
+      className={className}
+      style={{
+        width: '100%',
+        backgroundColor: colors.secondary,
+        borderColor: colors.border,
+        borderWidth: '1px',
+        borderStyle: 'solid',
+        height: '56px',
+        padding: '18px',
+        borderRadius: '14px',
+      }}
+      placeholder={placeholder}
+    />
+  )
+}
