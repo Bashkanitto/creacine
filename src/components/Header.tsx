5ef5d30ad093ef97945ae781d3d7068e398a8e22
aidyn <aidyn.meiramkhanuly@gmail.com>
2025-06-07 04:10:58 +0500

    feat: add header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import Link from 'next/link'
 import { CustomButton } from './CustomButton'
 import { useTranslations } from 'next-intl'
+import { Menu } from 'lucide-react'
 
 const Header = () => {
   const t = useTranslations('header')
   return (
-    <header className="fixed flex h-[61px] sm:h-[104px]  w-full max-w-[1240px] items-center justify-between rounded-4xl bg-white px-[25px] shadow-lg m-4 mt-[20px]">
+    <header className="fixed top-[20px] left-0 right-0 h-[61px] sm:h-[104px] w-[90%] xl:w-full max-w-[1240px] mx-auto flex items-center justify-between rounded-[30px] bg-white px-[25px] shadow-lg">
       <Link
         href="/"
         className="flex items-center justify-center text-6xl font-bold text-orange-500"
@@ -14,7 +15,7 @@ const Header = () => {
         2.47
       </Link>
       <nav className="hidden md:flex ">
-        <ul className="flex items-center gap-x-[35px] text-[13px] font-medium text-gray-600">
+        <ul className="flex items-center gap-x-[35px] text-[16px] font-medium text-gray-600">
           <li>
             <Link href="/about" className="hover:text-black">
               О нас
@@ -42,7 +43,14 @@ const Header = () => {
           </li>
         </ul>
       </nav>
-      <CustomButton type="button"> {t('discussProject')} </CustomButton>
+      <div className="flex items-center gap-2">
+        <CustomButton type="button" className="w-[220px] hidden md:flex">
+          {t('discussProject')}
+        </CustomButton>
+        <button className="flex md:hidden h-full">
+          <Menu />
+        </button>
+      </div>
     </header>
   )
 }
