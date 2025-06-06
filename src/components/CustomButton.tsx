e976b2ae1269d38a57513ff75728876a58db3d90
aidyn <aidyn.meiramkhanuly@gmail.com>
2025-06-07 04:11:35 +0500

    fix: add onclick props to customButton component

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -4,15 +4,18 @@ export function CustomButton({
   type,
   className,
   children,
+  onClick,
 }: {
   type: 'button' | 'submit' | 'reset'
   className?: string
   children?: React.ReactNode
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
 }) {
   return (
     <button
       type={type}
       className={className}
+      onClick={onClick}
       style={{
         backgroundColor: colors.primary,
         height: '43px',
