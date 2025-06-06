5739dd15b5d222d6583cb7eb568ddd69c9da510e
aidyn <aidyn.meiramkhanuly@gmail.com>
2025-06-06 23:15:16 +0500

    chore: add constant colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  primary: '#F76D3F', // Blue
+  secondary: '#EFEFEF', // Purple
+  success: '#4caf50', // Green
+  error: '#f44336', // Red
+  warning: '#ff9800', // Orange
+  info: '#2196f3', // Light Blue
+  background: '#f5f5f5', // Light Grey
+  text: '#212121', // Dark Grey
+  border: '#9E9E9E', // Dark Grey,
+}
