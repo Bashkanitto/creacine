583a12c13bca167ab8ca5d4537092b0bb6ceda9b
aidyn <aidyn.meiramkhanuly@gmail.com>
2025-06-07 20:14:03 +0500

    feat: add form page

---
+++ src/app/[locale]/form/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import { FeedbackForm } from '@/features/feedbackForm/FeedbackForm'
+
+export default function FormPage() {
+  return (
+    <div className="bg-[url('/images/formBackground.png')] bg-no-repeat bg-cover">
+      <Header />
+      <div className="pt-[100px]">
+        <FeedbackForm noBackground />
+      </div>
+      <Footer />
+    </div>
+  )
+}
