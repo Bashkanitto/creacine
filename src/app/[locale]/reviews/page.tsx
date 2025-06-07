1be213a031678d974c15e8017462d340ee47c2cc
aidyn <aidyn.meiramkhanuly@gmail.com>
2025-06-07 20:40:09 +0500

    feat: add reviews page

---
+++ src/app/[locale]/reviews/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import { FeedbackForm } from '@/features/feedbackForm/ui'
+import ReviewsComponent from '@/features/reviews/ui'
+
+export default function ReviewPage() {
+  return (
+    <div className="w-[1440px] mx-auto">
+      <Header />
+      <div className="pt-[200px] w-[88%] mx-auto">
+        <ReviewsComponent />
+      </div>
+      <div className="mt-[100px]">
+        <FeedbackForm />
+      </div>
+      <Footer />
+    </div>
+  )
+}
