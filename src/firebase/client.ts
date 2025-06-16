c96044c7fbd3213c1260b38e3407f91f40df6d89
aidyn <aidyn.meiramkhanuly@gmail.com>
2025-06-17 00:40:41 +0500

    fix: firebase/client.ts change app connectino state

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -14,7 +14,7 @@ const firebaseConfig = {
 }
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig)
 
 export const auth = getAuth(app)
 export const db = getFirestore(app)
