090dce0917160cd2f0329844fca9c45e3f20636f
aidyn <aidyn.meiramkhanuly@gmail.com>
2025-06-17 00:32:06 +0500

    feat: add responsivenes for not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,18 +1,19 @@
 'use client'
 import { CustomButton } from '@/components/CustomButton'
-import { colors } from '@/constants/colors'
+import { colors } from '@/shared/constants/colors'
 import { Home } from 'lucide-react'
+import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 
 export default function NotFound() {
   const router = useRouter()
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center p-4 justify-center min-h-screen">
       <h2>Ой, кажется, тут ничего нет</h2>
       <h2 style={{ color: colors.text }}>Зато на главной - всё, что нужно</h2>
-      <img src="/images/404.png" alt="404" />
-      <CustomButton type="button" className="p-2 flex gap-2" onClick={() => router.back()}>
+      <Image quality={75} width={975} height={515} src="/images/404.png" alt="404" />
+      <CustomButton type="button" className="p-2 flex gap-2" onClick={() => router.push('/')}>
         <Home />
         Вернутся на главную
       </CustomButton>
