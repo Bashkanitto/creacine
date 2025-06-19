9128c1d341ea0af62d45b0c14cee11dc1a9c502b
aidyn <aidyn.meiramkhanuly@gmail.com>
2025-06-19 05:03:23 +0500

    feat: add fonts

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontSize: {
+        base: '30px',
+        sm: '14px',
+        md: '18px',
+        lg: '20px',
+        xl: '30px',
+        '2xl': '32px',
+        '3xl': '58px',
+        '4xl': '72px',
+      },
+      fontFamily: {
+        loose: ['LooseWide', 'sans-serif'],
+        ivy: ['IvyPrestoDisplay', 'serif'],
+      },
+    },
+  },
+}
